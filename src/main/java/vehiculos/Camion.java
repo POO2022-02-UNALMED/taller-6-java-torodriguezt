4420c504bbb7d3606c93bc09ba98ed5b4ed4a97e
Tomás Rodríguez <torodriguezt@unal.edu.co>
2022-09-29 18:15:00 -0500

    Taller

--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -3,7 +3,7 @@ package vehiculos;
 public class Camion extends Vehiculo {
 	public int ejes;
 	
-	Camion(String placa, String nombre, int peso, int precio, Fabricante fabricante, int ejes){
+	public Camion(String placa, String nombre, int peso, int precio, Fabricante fabricante, int ejes){
 		super(placa, 2, 80, nombre, precio, peso, "4X2",fabricante);
 		this.ejes = ejes;
 	}
