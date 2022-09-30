c9824734e0b55eb36658243f82ea9a4b7b0c7cae
Tomás Rodríguez <torodriguezt@unal.edu.co>
2022-09-29 21:07:34 -0500

    Taller

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,12 +1,16 @@
 package vehiculos;
 
+import java.util.ArrayList;
+
 public class Fabricante {
 	public String nombre;
 	public Pais pais;
+	public ArrayList<String> fabricas = new ArrayList<String>();
 	
 	public Fabricante(String nombre, Pais pais) {
 		this.nombre = nombre;
 		this.pais = pais;
+		fabricas.add(this.nombre);
 	}
 	
 	public String getNombre() {
@@ -20,5 +24,26 @@ public class Fabricante {
 	}
 	public void setPais(Pais pais) {
 		this.pais = pais;
+	}
+	
+	public String fabricaMayorVentas() {
+		int cantidad = 0;
+		String maximo = null;
+		
+		for (int i = 0; i<fabricas.size(); i++ ) {
+			int contador = 0;
+			for(int j = 0; j<fabricas.size();j++) {
+				if (fabricas.get(i) == fabricas.get(j)) {
+					contador++;
+				}
+			}
+			
+			if (contador > cantidad) {
+				cantidad = contador;
+				maximo = fabricas.get(i);
+			}
+		}
+		
+		return maximo;
 	}
 }
--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,7 +1,10 @@
 package vehiculos;
 
+import java.util.ArrayList;
+
 public class Pais {
 	public String nombre;
+	public ArrayList<String> paises = new ArrayList<String>();
 	
 	public Pais(String nombre){
 		this.nombre = nombre;
@@ -14,4 +17,26 @@ public class Pais {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	public String paisMasVendedor() {
+		int cantidad = 0;
+		String maximo = null;
+		
+		for (int i = 0; i<paises.size(); i++ ) {
+			int contador = 0;
+			for(int j = 0; j<paises.size();j++) {
+				if (paises.get(i) == paises.get(j)) {
+					contador++;
+				}
+			}
+			
+			if (contador > cantidad) {
+				cantidad = contador;
+				maximo = paises.get(i);
+			}
+		}
+		
+		return maximo;
+	
+}
 }
